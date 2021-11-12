fc61bc8fa0691757ac84a8a14f232d3d4ff4e9a7
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-11-12 10:43:49 +0100

    v0.8
    Add comments to JS code.
    Edit CSS to resize game on smaller screens.

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ var gameSequence = [];
 // Boolean var to start the game
 var gameStarted = false;
 // Status var to determine the current state of the game
-var gameStatus = "pick";
+var gameStatus = "PICK_KEY";
 // Array used to store the sequence of keys the player is performing.
 var playerSequence = [];
 // Variable used to compare game and player sequences key by key
@@ -26,57 +26,63 @@ $(".key").click(function() {
   // If game is not started, switch the boolean to true and call game function
   if (gameStarted === false) {
     gameStarted = true;
-    score = 0;
-    $(".score").text(score);
+    // Hide score div
+    $(".scores").slideUp();
+
+    resetScore();
     game();
+
   }
   // If game is started
   else {
     // Get the class list of the key that triggered the event.
     // Then pick the second class only and store it in a variable to determine which key got pressed.
     var pressedKey = this.classList[1];
-    // Call playSound function and pass in the string.
+    // Call playSound function and pass in the string to play the sound corresponding to the key clicked by the user.
     playSound(pressedKey);
     // Call keyAnimation function to change key background color on click.
     keyAnimation(pressedKey);
 
-    if (gameStatus = "player_sequence") {
+    // If it is user's turn
+    if (gameStatus = "PLAYER_SEQUENCE") {
+      // Add pressed key to the playerSequence array
       playerSequence.push(pressedKey);
-
+      // Compare key clicked by user with computer key to check if they match
+      // If they dont match the user made a mistake
       if (playerSequence[currentPlayerKey] !== gameSequence[currentPlayerKey]) {
-
         $("h2").text("You made a mistake...");
-
-
+        // Check if user score is higher than previous saved highest score
         if (score > $(".hi-score").text()) {
+          // If so update highest score text
           $(".hi-score").text(score);
         }
 
-        // Display score div
+        // Show score div
         $(".scores").slideDown();
 
         resetGame();
-
+        // Exit current function
         return;
-        // Add game reset
 
       }
 
+      // If both keys are matching increment index value to check the next key the user will click on
       currentPlayerKey++;
 
+      // If we reached the end of the game sequence it means the user succeeded
       if (currentPlayerKey === gameSequence.length) {
         $("h2").text("Well done!");
-        gameStatus = "pick";
+        gameStatus = "PICK_KEY";
+        // Reset currentPlayerKey and user sequence array for the next turn
         currentPlayerKey = 0;
         playerSequence = [];
 
-
         incrementScore();
 
+        // Call game function after 500ms to let computer add a new key to the sequence
         setTimeout(function() {
           game();
         }, 500);
-
 
       }
     }
@@ -84,6 +90,15 @@ $(".key").click(function() {
   }
 
 });
+
+
+// Hide score menu when "Play again" is clicked
+$(".replay-button").click(function() {
+
+  $(".scores").slideUp();
+
+});
+
 
 // Starts playing the audio corresponding to the received key
 function playSound(key) {
@@ -120,17 +135,17 @@ function keyAnimation(key) {
 }
 
 
-
-
+// Deal with computer parts of the game
 function game() {
-
-  if (gameStatus === "pick") {
+  // Pick a new key to add to the game sequence
+  if (gameStatus === "PICK_KEY") {
 
     pickNewKey();
 
   }
 
-  if (gameStatus === "bot_sequence") {
+  // Play the game sequence the user will have to reproduce
+  else if (gameStatus === "BOT_SEQUENCE") {
 
     playSequenceToReproduce();
 
@@ -139,6 +154,7 @@ function game() {
 }
 
 
+// Pick a new key to add to the game sequence
 function pickNewKey() {
   $("h2").text("Listen carefully...");
 
@@ -146,12 +162,17 @@ function pickNewKey() {
   randomKey = keys[Math.floor(Math.random() * keys.length)];
   gameSequence.push(randomKey);
 
-  gameStatus = "bot_sequence";
+  gameStatus = "BOT_SEQUENCE";
+
+  game();
+
 }
 
 
+// Play every single key of the bot sequence
 function playSequenceToReproduce() {
-  // Play every single key of the bot sequence
+
+  // Plays a key from the bot sequence then plays the next key after 1s until we reach the end of the sequence
   setTimeout(function() {
 
     // Call playSound function and pass in the string.
@@ -159,14 +180,20 @@ function playSequenceToReproduce() {
     // Call keyAnimation function to change key background color on click.
     keyAnimation(gameSequence[keysCount]);
 
+    // Increment current key index
     keysCount++;
 
+    // If there are keys left to play in the sequence
     if (keysCount < gameSequence.length) {
-      game();
-    } else {
+      // Call the function again
+      playSequenceToReproduce();
+    }
+    // If we reached the end of the sequence
+    else {
+      // Reset key index and allow player to play
       keysCount = 0;
-      gameStatus = "player_sequence";
-
+      gameStatus = "PLAYER_SEQUENCE";
+      // Display a message to the user to let him know
       setTimeout(function() {
         $("h2").text("Your turn!");
       }, 500);
@@ -176,36 +203,34 @@ function playSequenceToReproduce() {
   }, 1000);
 }
 
-
+// Add 1 to user score and update displayed value
 function incrementScore() {
   score++;
   $(".score").text(score);
 }
 
 
+// Set user score value to 0 and update displayed value
+function resetScore() {
+  score = 0;
+  $(".score").text(score);
+}
 
+
+// Reset game state to start a new one
 function resetGame() {
 
-
+  // After 500ms reset all behavior variables to their init values and display starting h2 message.
   setTimeout(function() {
 
     keysCount = 0;
-    gameSequence = [];
-    gameStarted = false;
-    gameStatus = "pick";
-    playerSequence = [];
     currentPlayerKey = 0;
+    gameSequence = [];
+    playerSequence = [];
+    gameStarted = false;
+    gameStatus = "PICK_KEY";
     $("h2").text("Click on any key to start");
 
   }, 500);
 
-
 }
-
-
-
-$(".replay-button").click(function() {
-
-  $(".scores").slideUp();
-
-});
